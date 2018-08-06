f1867752532c266e86195291e106a2e882004abc
Laimis Valatka <laimison1@gmail.com>
2018-08-06 16:45:41 +0100

    Switched to forked version

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ echo " -=- Getting software -=-"
 sudo apt-get update
 sudo apt-get -y install libav-tools screen lighttpd php php-cgi git
 cd /opt
-sudo git clone https://github.com/Fmstrat/diy-ipmi
+# Changed to fork
+# sudo git clone https://github.com/Fmstrat/diy-ipmi
+sudo git clone https://github.com/laimison/diy-ipmi.git
 sudo chown pi diy-ipmi -R
 chmod +x /opt/diy-ipmi/Pi3/*.py
 chmod +x /opt/diy-ipmi/Pi3/*.sh
